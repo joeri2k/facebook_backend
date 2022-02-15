806c146637ae510b63673551eb9530648c31e3e4
joe rizk <joerizk2@hotmail.com>
2022-02-16 00:13:35 +0200

    writing query for login and sending encrypted id

--- login.php
+++ login.php
@@ -12,5 +12,27 @@ if(isset($_GET["email"]) && isset($_GET["password"])){
     die("Your Email or Password are missing."); 
 }
 
+// getting query result
+$query=$mySqli ->prepare("SELECT ID FROM users WHERE email=? AND password=? ");
+$query ->bind_param("ss", $email, $password);
+$query -> execute();
+$query -> store_result();
+$query->bind_result($id);
+$num_rows = $query ->num_rows;
+$query ->fetch();
 
+// checking user availability
+if($num_rows==0){
+    $array_response = ["status" => "Email or Password is incorrect !"];
+} else{
+    $array_response = ["status" => "logged in!"];
+    $encrypted_user_id = encryption($id);
+    $array = ["user_id" => $encrypted_user_id];
+    echo json_encode($array);
+}
+
+$json_response=json_encode($array_response);
+echo $json_response;
+$query ->close();
+$mySqli -> close();
 ?>
