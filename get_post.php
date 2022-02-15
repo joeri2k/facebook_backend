aaa76719694689275bf4393196b309ac82d309c5
joe rizk <joerizk2@hotmail.com>
2022-02-16 00:22:15 +0200

    api to get all the posts of friends and user

---
+++ get_post.php
@@ -0,0 +1,55 @@
+<?php
+
+include("db_info.php");
+include("usable_functions.php");
+
+$user_id = $_GET['user_id'];
+//decryption($user_id)
+
+$query=$mySqli ->prepare("SELECT users.ID, users.first_name, users.family_name, 
+post.ID,post.content_of_Post, post.Date_of_Post 
+FROM post
+Inner Join friends On post.Users_ID = friends.Users_recieved_ID
+Inner Join users On post.Users_ID = users.ID
+Where friends.Status_of_Request = 'confirmed' And friends.Users_sent_ID = ?
+UNION
+SELECT users.ID, users.first_name, users.family_name, post.ID,post.content_of_Post, post.Date_of_Post 
+FROM post
+Inner Join friends On post.Users_ID = friends.Users_sent_ID
+Inner Join users On post.Users_ID = users.ID
+Where friends.Status_of_Request = 'confirmed' And friends.Users_recieved_ID = ?
+UNION
+SELECT users.ID, users.first_name, users.family_name, 
+post.ID,post.content_of_Post, post.Date_of_Post 
+FROM post
+Inner Join users On post.Users_ID = users.ID
+Where  users.ID = ?
+ORDER BY Date_of_Post DESC");
+
+$query ->bind_param("sss", $user_id,$user_id, $user_id);
+$query -> execute();
+$query -> store_result();
+$query->bind_result($userID,$firstname,$familyname,$postID,$postcontent,$postDate);
+$num_rows = $query ->num_rows;
+
+if($num_rows==0){
+    $array_response["status"]= "Friends not found !";
+} else{
+    //iterate over the result retrieving one row at a time 
+    $count = 0;
+    while ($row = $query->fetch())
+    {
+        //buffer the row onto $data
+        $array_response["result-".$count]= ["first_name" => $firstname,
+        "family_name" => $familyname,
+        "contentPost" => $postcontent];
+        $count = $count + 1;
+
+    }
+}
+
+$json_response=json_encode($array_response);
+echo $json_response;
+$query ->close();
+$mySqli -> close();
+?>
