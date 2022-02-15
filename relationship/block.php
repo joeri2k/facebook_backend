f6f8f80aae5dad02bfe0e703c78467e97cd45c08
joe rizk <joerizk2@hotmail.com>
2022-02-16 01:05:29 +0200

    api to block

---
+++ relationship/block.php
@@ -0,0 +1,54 @@
+<?php
+
+include("db_info.php");
+include("usable_functions.php");
+
+$my_id = $_GET['my_id'];
+//decryption($user_id)
+$user_id = $_GET['user_id'];
+
+$query1=$mySqli ->prepare("SELECT * FROM friends 
+WHERE (Users_sent_ID = ? AND Users_recieved_ID = ?) 
+OR ( Users_recieved_ID= ? AND Users_sent_ID = ?)");
+$query1 ->bind_param("ssss", $my_id, $user_id, $my_id ,$user_id);
+$query1 -> execute();
+$query1 -> store_result();
+$num_rows1 = $query1 ->num_rows;
+$query1 ->fetch();
+
+// checking user availability
+if($num_rows1 !=0){
+    $query = $mySqli ->prepare("UPDATE friends 
+    SET  Status_of_Request = 'blocked',
+        Users_sent_ID = ?,
+        Users_recieved_ID = ?
+    WHERE (Users_sent_ID = ? AND Users_recieved_ID = ?)");
+    $query ->bind_param("ssss", $my_id,$user_id, $user_id, $my_id);
+    $query -> execute();
+    $query -> store_result();
+} else{
+
+$query = $mySqli ->prepare("INSERT INTO friends(where Users_sent_ID, Users_recieved_ID, Status_of_Request) 
+VALUES(?,?,'blocked')");
+$query ->bind_param("ss", $my_id, $user_id);
+$query -> execute();
+$query -> store_result();
+$query ->get_result();
+}
+
+// print_r($query);
+$array_response=[];
+if ($query -> affected_rows == 1){
+    
+    $array_response = ["result" => "successful"];
+
+}
+else {
+    $array_response = ["result" => "unsuccessful"];
+}
+
+$json_response=json_encode($array_response);
+echo $json_response;
+$query ->close();
+$mySqli -> close();
+?>
